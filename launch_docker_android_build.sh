ee151235d5f72682d344b4a7cb2768d09397a286
Lawrence Spector <lspector@peerstream.com>
2019-04-18 15:50:21 -0400

    Run docker in privileged mode

--- launch_docker_android_build.sh
+++ launch_docker_android_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v `pwd`:`pwd` -w `pwd` -i -t ubuntu `pwd`/start_docker_build.sh
+docker run --privileged -v `pwd`:`pwd` -w `pwd` -i -t ubuntu `pwd`/start_docker_build.sh

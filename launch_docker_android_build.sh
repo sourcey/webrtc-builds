09bf409684165e1fd26b8c1bcffe01492cc6d9af
Lawrence Spector <lspector@peerstream.com>
2019-04-18 21:50:06 -0400

    Launch ubuntu 16.04

--- launch_docker_android_build.sh
+++ launch_docker_android_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --privileged -v `pwd`:`pwd` -w `pwd` -i -t ubuntu `pwd`/start_docker_build.sh
+docker run --privileged -v `pwd`:`pwd` -w `pwd` -i -t ubuntu:16.04 `pwd`/start_docker_build.sh
